dcad3dd9b7d7cdc8a64dffa5400ada3510c056c5
harikrishnan <harimee1504@gmail.com>
2025-02-19 20:57:19 +0530

    console statement added for debugging

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
+    console.log('Clerk Publishable Key', process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY)
     return (
         <Component {...pageProps} />
     );
